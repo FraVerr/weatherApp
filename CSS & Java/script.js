2fb8914d4b8021d65c813fd2f3d22ea44b0e0c7d
FraVerr <verrillo.francesca@gmail.com>
2023-03-19 11:21:22 +0100

    Added forecast structure

--- CSS & Java/script.js
+++ CSS & Java/script.js
@@ -24,11 +24,34 @@ let changeDayHours = document.querySelector("#currentDate");
 changeDayHours.innerHTML = `${day} ${hour}:${minutes}`;
 
 //Forecast
-function getForecast(coordinates) {
-  let apiKey = "bf54175800a55e59e6c4d6461deeef12";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-  axios.get(apiUrl).then(displayForecast);
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `        
+          <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+            
+            <img
+              src="http://openweathermap.org/img/wn/50d@2x.png"
+              alt=""
+              width="36"
+            />
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temp-max"> 18° </span>
+              <span class="weather-forecast-temp-min"> 12° </span>
+            </div>
+          </div>
+        
+        `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 // Search Engine
@@ -93,6 +116,8 @@ let celsiusLink = document.querySelector("#celsiusTemp");
 celsiusLink.addEventListener("click", showCelsius);
 
 search("Milan");
+
+displayForecast();
 
 //Current Location
 function currentLocation(event) {
