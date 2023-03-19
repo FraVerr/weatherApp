2902e9c0983313bf2d8b387548b65f33730befb2
FraVerr <verrillo.francesca@gmail.com>
2023-03-19 11:32:23 +0100

    Added forecast URL & created function

--- CSS & Java/script.js
+++ CSS & Java/script.js
@@ -24,7 +24,8 @@ let changeDayHours = document.querySelector("#currentDate");
 changeDayHours.innerHTML = `${day} ${hour}:${minutes}`;
 
 //Forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -54,6 +55,13 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9eca7aac0b071aa16e3cb063adba0785";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 // Search Engine
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
@@ -75,6 +83,8 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   emojiElement.getAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function handleSubmit(event) {
